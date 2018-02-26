0702db6abb5df4e553734252650a54c4d2c0c47c
kamildydo <dydokamil@gmail.com>
2018-02-26 12:31:00 +0100

    Background stays fixed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg'
+import './App.css'
 
 const Slide = ({ children, ...props }) => (
   <CSSTransition {...props} timeout={100000} classNames="slideBack">
     {children}
   </CSSTransition>
-);
+)
 
 class App extends Component {
   constructor(...args) {
-    super(...args);
-    this.state = { show: false };
+    super(...args)
+    this.state = { show: false }
 
     setTimeout(() => {
-      this.setState({ show: !this.state.show });
-    }, 0);
+      this.setState({ show: !this.state.show })
+    }, 0)
   }
 
   render() {
     return (
-      <Slide in={this.state.show}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Slide in={this.state.show}>
             <h1 className="App-title">Welcome to React</h1>
-          </header>
+          </Slide>
+        </header>
+        <Slide in={this.state.show}>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <Link to="/app-fade">Next</Link>
-        </div>
-      </Slide>
-    );
+        </Slide>
+        <Link to="/app-fade">Next</Link>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
--- src/AppSlide.js
+++ src/AppSlide.js
@@ -1,49 +1,51 @@
-import React, { Component } from "react";
-import { Transition } from "react-transition-group";
-import { Link } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React, { Component } from 'react'
+import { Transition } from 'react-transition-group'
+import { Link } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg'
+import './App.css'
 
 const Fade = ({ children, ...props }) => (
   <CSSTransition {...props} timeout={100000} classNames="fade">
     {children}
   </CSSTransition>
-);
+)
 
 const Slide = ({ children, ...props }) => (
   <CSSTransition {...props} timeout={100000} classNames="slide">
     {children}
   </CSSTransition>
-);
+)
 
 class AppSlide extends Component {
   constructor(...args) {
-    super(...args);
-    this.state = { show: false };
+    super(...args)
+    this.state = { show: false }
 
     setTimeout(() => {
-      this.setState({ show: !this.state.show });
-    }, 0);
+      this.setState({ show: !this.state.show })
+    }, 0)
   }
 
   render() {
     return (
-      <Slide in={this.state.show}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Slide in={this.state.show}>
             <h1 className="App-title">Welcome to React</h1>
-          </header>
+          </Slide>
+        </header>
+        <Slide in={this.state.show}>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <Link to="/">Previous</Link>
-        </div>
-      </Slide>
-    );
+        </Slide>
+        <Link to="/">Previous</Link>
+      </div>
+    )
   }
 }
 
-export default AppSlide;
+export default AppSlide
